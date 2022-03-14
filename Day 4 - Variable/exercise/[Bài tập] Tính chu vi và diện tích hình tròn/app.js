33d060f0cc171a141afebc9b1adc7774c7516a88
Nguyenkhanh284 <yawu232014@gmail.com>
2022-03-14 12:57:41 +0700

    Signed-off-by: Nguyenkhanh284 <yawu232014@gmail.com>

---
+++ Day 4 - Variable/exercise/[Bài tập] Tính chu vi và diện tích hình tròn/app.js
@@ -0,0 +1,10 @@
+let Radius = prompt('Mời bạn hhập bán khính hình tròn');
+const PI = 3.14;
+let Diameter = 2*Radius;
+let C = Diameter*2
+let S = (Radius**2)*PI;
+document.write('Đường kính hình tròn là:'+' '+Diameter);
+document.write('<br>');
+document.write('Chu vi hình tròn là:'+' '+C);
+document.write('<br>');
+document.write('Diện tích hình tròn là:'+' '+S);
