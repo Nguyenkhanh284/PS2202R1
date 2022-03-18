d1e85c9f5f2294b4dcc4452398bebcc36ed08271
Nguyenkhanh284 <yawu232014@gmail.com>
2022-03-18 12:36:26 +0700

    Signed-off-by: Nguyenkhanh284 <yawu232014@gmail.com>

---
+++ Day 5-HTML DOM/[Thực hành] Kiểm tra năm nhuận/app.js
@@ -0,0 +1,15 @@
+function checkYear() {
+    var year = document.getElementById('year').value;
+    var check = parseFloat(year);
+    if (check % 4 == 0) {
+        if (check % 100 !== 0) {
+           alert(year + ' ' + 'la nam nhuan');
+        } else if (check % 400 !== 0){
+           alert(year + ' ' + 'khong phai la nam nhuan')
+        } else {
+            alert(year + ' ' + 'la nam nhuan'); 
+        }
+    } else {
+        alert(year + ' ' + 'khong phai la nam nhuan')
+    }
+}
