091b768691b90e7ba8f21d7b971cb6b3429c2404
Nguyenkhanh284 <yawu232014@gmail.com>
2022-03-27 17:04:04 +0700

    Signed-off-by: Nguyenkhanh284 <yawu232014@gmail.com>

---
+++ day 7 Loop/[Bài tập] In dãy Fibonacci/app.js
@@ -0,0 +1,16 @@
+let inputNumb = parseFloat(prompt('Moi nhap so nguyen duong'));
+let numb1 = 1;
+let numb2 = 1;
+let numbN
+for (let i = 1; i <= inputNumb; i++) {
+    if (i == 1) {
+        console.log(numb1);
+    } else if (i == 2) {
+        console.log(numb2);
+    } else {
+        numbN = numb1 + numb2;
+        numb1 = numb2;
+        numb2 = numbN;
+        console.log(numbN);
+    }  
+}
