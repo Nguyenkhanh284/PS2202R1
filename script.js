fe0825d5e4a6f07475be0442885eaa7dd2d28b47
Nguyenkhanh284 <yawu232014@gmail.com>
2022-04-24 15:45:03 +0700

    Signed-off-by: Nguyenkhanh284 <yawu232014@gmail.com>

--- script.js
+++ script.js
@@ -2,34 +2,39 @@
  * Created by nhatnk on 4/26/17.
  */
 
-function Hero(image, top, left, size){
+var speedCar;
+function Hero(image, top, left, size) {
   this.image = image;
   this.top = top;
   this.left = left;
   this.size = size;
+  this.speed = parseFloat(prompt('Do you want speed'));
 
-  this.getHeroElement = function(){
-    return '<img width="'+ this.size + '"' +
-      ' height="'+ this.size + '"' +
-      ' src="' + this.image +'"' +
-      ' style="top: '+this.top+'px; left:'+this.left+'px;position:absolute;" />';
+
+  this.getHeroElement = function () {
+    return '<img width="' + this.size + '"' +
+      ' height="' + this.size + '"' +
+      ' src="' + this.image + '"' +
+      ' style="top: ' + this.top + 'px; left:' + this.left + 'px;position:absolute;" />';
   }
 
-  this.moveRight = function(){
-    this.left += 20;
-    console.log('ok: ' + this.left);
+  this.moveRight = function () {
+    this.left += this.speed;
+    console.log('qua phai: ' + this.left);
   }
 
 }
 
-var hero = new Hero('pikachu.png', 20, 30, 200);
+var hero = new Hero('car.png', 10, 40, 100);
 
-function start(){
-  if(hero.left < window.innerWidth - hero.size){
-    hero.moveRight();
+function start() {
+  if (hero.left < window.innerWidth - hero.size && hero.left > 0) {
+    hero.speed = hero.speed;
+  } else {
+    hero.speed = -hero.speed;
   }
+  hero.moveRight();
   document.getElementById('game').innerHTML = hero.getHeroElement();
-  setTimeout(start, 500)
+  setTimeout(start, 50)
 }
-
 start();
